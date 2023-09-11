8440a3f66477c0f65223d707acc2ff74fcf6ce70
technologicaljerry <cserajatverma@gmail.com>
2023-09-11 21:54:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ app.use(express.json());
 const serverName = process.env.SERVER_NAME;
 
 const serverPort = process.env.SERVER_PORT | 5050;
+
+app.listen(serverPort, () => {
+    console.log(`${serverName} SERVER runnibg on PORT: ${serverPort}`);
+});
